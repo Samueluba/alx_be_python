fa8b39c3ddaca80f373066c778102b7c7918a1a8
Samueluba <111093750+Samueluba@users.noreply.github.com>
2025-09-16 10:59:32 +0100

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,1 +1,11 @@
+userque = input("What's the weather like today? (sunny/rainy/cold):")
+if userque == 'sunny':
+    print("the weather is “sunny”, recommend: Wear a t-shirt and sunglasses.")
+if userque == 'rainy':
+     print("The weather is rainy, recommend: Don't forget your umbrella and a raincoat.")
+elif userque == "cold":
+    print("The weather is cold, recommend: Wear a jacket and scarf.")
+else:
+    print("Sorry, I don't have a recommendation for that weather.")
 
+                
